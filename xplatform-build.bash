202099202c95b3d878822ebb77d379b62ab05259
Sasha Varlamov <sasha@sashavarlamov.com>
2017-11-23 00:59:43 +0800

    Create xplatform build script

---
+++ xplatform-build.bash
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+rm -rf ./build
+GOOS=darwin
+GOARCH=amd64
+go build -o $(echo ./build/$GOOS-$GOARCH/mdlr)
+GOOS=darwin
+GOARCH=386
+go build -o $(echo ./build/$GOOS-$GOARCH/mdlr)
+GOOS=linux
+GOARCH=amd64
+go build -o $(echo ./build/$GOOS-$GOARCH/mdlr)
+GOOS=linux
+GOARCH=386
+go build -o $(echo ./build/$GOOS-$GOARCH/mdlr)
+GOOS=windows
+GOARCH=amd64
+go build -o $(echo ./build/$GOOS-$GOARCH/mdlr.exe)
+GOOS=windows
+GOARCH=386
+go build -o $(echo ./build/$GOOS-$GOARCH/mdlr.exe)
