1fa367389be4031137df2529dae0bcfee87dc55d
Sasha Varlamov <sasha@sashavarlamov.com>
2017-11-24 15:06:18 +0800

    Start creating an install.sh script

---
+++ installers/install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+cd /usr/local/bin
+URL='https://git.exlhub.io/exlinc/tools-mdlr/releases/todo/'
+MACHINE_TYPE=`uname -m`
+ARCH='386'
+OS='linux'
+if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+    ARCH='amd64'
+fi
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    OS='darwin'
+fi
+echo "Downloading mdlr"
+curl -o mdlr ${URL}${OS}-${ARCH}-bin
+echo "Installed"
