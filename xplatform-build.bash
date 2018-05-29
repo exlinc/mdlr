57d4abffceef47f37c220dcb08559b1a30810d34
Sasha Varlamov <sasha@sashavarlamov.com>
2018-05-29 20:35:47 +0800

    Fix env var error in x-platform build

--- xplatform-build.bash
+++ xplatform-build.bash
@@ -3,21 +3,22 @@ if [ ! -d "./build" ]; then
   echo "Use mdlr to import the ./build directory with mdlr import"
   exit 1
 fi
+
 GOOS=darwin
 GOARCH=amd64
-go build -o $(echo ./build/$GOOS-$GOARCH-mdlr)
+GOOS=$GOOS GOARCH=$GOARCH go build -o $(echo ./build/$GOOS-$GOARCH-mdlr)
 GOOS=darwin
 GOARCH=386
-go build -o $(echo ./build/$GOOS-$GOARCH-mdlr)
+GOOS=$GOOS GOARCH=$GOARCH go build -o $(echo ./build/$GOOS-$GOARCH-mdlr)
 GOOS=linux
 GOARCH=amd64
-go build -o $(echo ./build/$GOOS-$GOARCH-mdlr)
+GOOS=$GOOS GOARCH=$GOARCH go build -o $(echo ./build/$GOOS-$GOARCH-mdlr)
 GOOS=linux
 GOARCH=386
-go build -o $(echo ./build/$GOOS-$GOARCH-mdlr)
+GOOS=$GOOS GOARCH=$GOARCH go build -o $(echo ./build/$GOOS-$GOARCH-mdlr)
 GOOS=windows
 GOARCH=amd64
-go build -o $(echo ./build/$GOOS-$GOARCH-mdlr.exe)
+GOOS=$GOOS GOARCH=$GOARCH go build -o $(echo ./build/$GOOS-$GOARCH-mdlr.exe)
 GOOS=windows
 GOARCH=386
-go build -o $(echo ./build/$GOOS-$GOARCH-mdlr.exe)
+GOOS=$GOOS GOARCH=$GOARCH go build -o $(echo ./build/$GOOS-$GOARCH-mdlr.exe)
